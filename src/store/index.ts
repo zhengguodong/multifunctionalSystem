ab0d9e3b41fa58a8724154e2fea16d96fff904fd
zhengguodong <571954027@qq.com>
2022-09-08 20:59:36 +0800

    vuex持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@
 // 使用vuex的createStore创建store实例
 // 导出useStore方法，返回值Store，其他地方需要用到store的一定要使用useStore()
 import { createStore, createLogger } from 'vuex'
+import createPersistedState from "vuex-persistedstate"
 import { store as app, AppStore, AppState } from '@/store/modules/app'
 import {main} from "./modules/main/index"
 export interface RootState {
@@ -13,7 +14,18 @@ export type Store = AppStore<Pick<RootState, 'app'>>
 
 // Plug in logger when in development environment
 const debug = process.env.NODE_ENV !== 'production'
-const plugins = debug ? [createLogger({})] : []
+// const plugins = debug ? [createLogger({})] : []
+let plugins :any;
+plugins.push([createPersistedState({
+  storage: window.sessionStorage,
+  reducer(val) {
+    return {
+      // 只储存state中的assessmentData
+      assessmentData: val.assessmentData
+    }
+  }
+})]
+)
 export const store = createStore({
   plugins,
   modules: {
